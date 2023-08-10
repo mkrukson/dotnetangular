3a2c113e40a9821cc4da92b616024daceeffe6f2
Marcel Krukowski <mkrukson@gmail.com>
2023-08-10 15:24:52 +0200

    End of section 2

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,15 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
